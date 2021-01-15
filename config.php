25a47a1127be876399c51f75c640079a54abe832
idpandam <77486789+idpandam@users.noreply.github.com>
2021-01-15 12:15:51 +0100

    config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'pandam14');
+define('PREFIX', 'pa');
+define('DB_CHARSET', 'utf8');
+define('DATAROOT', dirname(__FILE__));
+define('INC','/includes');
+?>
